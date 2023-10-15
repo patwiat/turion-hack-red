9cc188ec83b1332d0f7fc5bd629ddd3cbe990869
Your Name <you@example.com>
2023-10-15 14:26:13 -0700

    add login components

--- src/Login/login.jsx
+++ src/Login/login.jsx
@@ -1,11 +1,27 @@
+
+import React from "react";
 import "./login.css";
 
 const Login = () => {
   return (
-    <>
-      <p>Hi</p>
-      <button href="/home">Next</button>
-    </>
+    <div className="login-container">
+      <div className="transparent-box">
+        <p className="login-text">Login</p>
+        <p className="subtext">Explore the galaxy with us.</p>
+        <form>
+          <div className="form-group">
+            <label htmlFor="username">Username:</label>
+            <input type="text" id="username" name="username" />
+          </div>
+          <div className="form-group">
+            <label htmlFor="password">Password:</label>
+            <input type="password" id="password" name="password" />
+          </div>
+          <button type="submit">Next</button>
+        </form>
+      </div>
+    </div>
+
   );
 };
 
